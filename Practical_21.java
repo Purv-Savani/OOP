9be1f3fcc5ca9be4bac1fa50a802233e973835ac
Purv-Savani <purvsavani@gmail.com>
2026-04-01 20:16:29 +0530

    Implement voting eligibility check in VotingApp

---
+++ Practical_21.java
@@ -0,0 +1,29 @@
+class VotingApp
+{
+    void checkEligibility(int age)
+    {
+        if(age<18)
+            throw new IllegalArgumentException("Age must be 18 or above to vote");
+        else
+            System.out.println("Eligible to vote");
+    }
+
+    public static void main(String args[])
+    {
+        VotingApp v=new VotingApp();
+
+        try
+        {
+            v.checkEligibility(16);
+            v.checkEligibility(20);
+        }
+        catch(IllegalArgumentException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        finally
+        {
+            System.out.println("Validation process completed");
+        }
+    }
+    }
