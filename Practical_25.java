ac47f17f06f4f922bb9333e4204a38aa171d82ae
Purv-Savani <purvsavani@gmail.com>
2026-04-01 20:22:24 +0530

    Add synchronized table printing using threads
    Implement synchronized table printing with threads.

---
+++ Practical_25.java
@@ -0,0 +1,53 @@
+class Table
+{
+    synchronized void printTable(int n)
+    {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n+" x "+i+" = "+(n*i));
+            try{Thread.sleep(500);}catch(Exception e){}
+        }
+    }
+}
+
+class T1 extends Thread
+{
+    Table t;
+    T1(Table t)
+    {
+        this.t=t;
+    }
+
+    public void run()
+    {
+        t.printTable(5);
+    }
+}
+
+class T2 extends Thread
+{
+    Table t;
+    T2(Table t)
+    {
+        this.t=t;
+    }
+
+    public void run()
+    {
+        t.printTable(7);
+    }
+}
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Table obj=new Table();
+
+        T1 t1=new T1(obj);
+        T2 t2=new T2(obj);
+
+        t1.start();
+        t2.start();
+    }
+  }
